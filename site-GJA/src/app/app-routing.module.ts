8f54063c2c017d7b57543181046fde1e6a8ed650
taiello19 <thomasaiello19@gmail.com>
2023-05-30 12:30:55 -0400

    basic navbar without logo

--- site-GJA/src/app/app-routing.module.ts
+++ site-GJA/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  //routes by default to home page
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent}
 ];
